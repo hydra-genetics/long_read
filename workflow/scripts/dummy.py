3ea949414756f5384c30779e50820188d26c307c
magdalenazz <magdalenazz477@gmail.com>
2024-08-21 13:26:19 +0200

    bug: linting

---
+++ workflow/scripts/dummy.py
@@ -0,0 +1,11 @@
+# vim: syntax=python tabstop=4 expandtab
+# coding: utf-8
+
+__author__ = "Uppsala Universitet"
+__copyright__ = "Copyright 2024, Uppsala Universitet"
+__email__ = "magdalena.z@scilifelab.uu.se"
+__license__ = "GPL-3"
+
+
+def dummy():
+    return 1
